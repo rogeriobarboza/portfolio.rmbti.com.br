6402ca5e423f61d5504cd744597fa5e55dab794d
Rogerio Morais Barboza <rogeriomoraisbarboza@gmail.com>
2024-03-26 18:11:14 -0300

    Initial commit

---
+++ sobre.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>RMB Ti</title>
+    
+    <link rel="stylesheet" href="css/header.css">
+    <script src="js/header.js" defer></script>
+
+    <link rel="stylesheet" href="css/body.css">
+    <link rel="stylesheet" href="css/footer.css">
+
+</head>
+<body>
+
+<!-- divisão semântica cabeçalio-->
+<header>
+        <nav class="navbar">
+            <a href="index.php" class="logo">Rogerio Morais Barboza Ti</a>
+            <ul class="nav-menu">
+                <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+                <li class="nav-item"><a href="sobre.php" class="nav-link">Sobre</a></li>
+                <li class="nav-item"><a href="contato.php" class="nav-link">Contato</a></li>
+            </ul>
+            <div class="hamburguer">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </div>
+        </nav>
+    </header>
+<!-- divisão semântica corpo-->
+<div class="divBody">
+    <div class="divContBody">
+        <div class="divTxtBody">
+            <div class="txtBody">
+                <h1>SOBRE</h1><br>
+                <p>ROGERIO MORAIS BARBOZA<br>
+                42 anos<br>
+                Santo André/SP<br>
+                linkedin.com/in/rogeriomoraisbarboza<br>
+                github.com/rogeriobarboza
+                </p><br>
+                <h2>OBJETIVO:</h2>
+                <h2>Estágio</h2><br>
+                <p>FORMAÇÃO ACADÊMICA<br>
+                Cursando superior tecnólogo em análise e desenvolvimento de sistemas - UNICSUL
+conclusão em 2024.
+                </p>
+                
+            </div>
+        </div>
+    </div>
+</div>
+<!-- divisão semântica roda-pé-->
+<div class="divFooter">
+    <div class="divFooterContente">
+        <div class="divTxtFooter">
+            <p class="txtFooter">Rogerio Morais Barboza Ti</p>
+        </div>
+    </div>
+</div>
+    
+</body>
+</html>
